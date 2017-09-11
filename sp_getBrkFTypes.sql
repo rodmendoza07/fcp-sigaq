c91e275b890f47e95c640b2c2c7bdf5cec044e2a
rodrigo <luis.mendoza@grupoporvenir.com.mx>
2017-09-11 17:30:13 -0500

    se agregan sucursales a sp_getBrkFTypes

--- sp_getBrkFTypes.sql
+++ sp_getBrkFTypes.sql
@@ -1,11 +1,16 @@
 USE CATALOGOS
 GO
 
-CREATE PROCEDURE [dbo].[sp_getBrkFTypes]
+ALTER PROCEDURE [dbo].[sp_getBrkFTypes]
 AS
 BEGIN
 	SELECT *
 	FROM CATALOGOS.dbo.tc_brkFinanceType
+
+	SELECT 
+		dep.id_departamento
+		, REPLICATE('0', 5 - LEN(dep.id_departamento)) + CAST(dep.id_departamento AS varchar) + ' - ' + dep.descripcion AS branchOffice
+	FROM CATALOGOS.dbo.tc_departamento dep
 END
 
 --EXEC CATALOGOS.dbo.sp_getBrkFTypes
