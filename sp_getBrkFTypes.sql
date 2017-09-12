da866b0b96eae2aeb2fe3b05940b1bec616653f6
rodrigo <luis.mendoza@grupoporvenir.com.mx>
2017-09-12 11:04:18 -0500

    todos las areas son desplegadas en sp_gerBrkFtypes

--- sp_getBrkFTypes.sql
+++ sp_getBrkFTypes.sql
@@ -1,7 +1,7 @@
 USE CATALOGOS
 GO
 
-CREATE PROCEDURE [dbo].[sp_getBrkFTypes]
+ALTER PROCEDURE [dbo].[sp_getBrkFTypes]
 AS
 BEGIN
 	SELECT *
@@ -11,7 +11,7 @@ BEGIN
 		dep.id_departamento AS brk_boId
 		, REPLICATE('0', 5 - LEN(dep.id_departamento)) + CAST(dep.id_departamento AS varchar) + ' - ' + dep.descripcion AS brk_bo
 	FROM CATALOGOS.dbo.tc_departamento dep
-	WHERE dep.id_departamento < 300
+	--WHERE dep.id_departamento < 300
 END
 
 --EXEC CATALOGOS.dbo.sp_getBrkFTypes
