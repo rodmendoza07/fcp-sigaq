c1f93d063f5e0a62e2027bea08ff720735969541
rodrigo <luis.mendoza@grupoporvenir.com.mx>
2017-09-14 16:34:51 -0500

    se agrega sp_setBrkWarrantyToPay

--- tp_brkWarrantyToPay.sql
+++ tp_brkWarrantyToPay.sql
@@ -5,6 +5,7 @@ CREATE TABLE tp_brkWarrantyToPay (
 	brkwp_id INT NOT NULL IDENTITY(1,1)
 	, brkwp_origin VARCHAR(50) NOT NULL CONSTRAINT DEF_brkwp_origin DEFAULT('')
 	, brkwp_codeSva VARCHAR(20) NOT NULL CONSTRAINT DEF_brkwp_codeSva DEFAULT('')
+	, brkwp_branchOffice INT NOT NULL CONSTRAINT DEF_brkwp_branchOffice DEFAULT('')
 	, brkwp_amount DECIMAL(18,4) NOT NULL CONSTRAINT DEF_brkwp_amount DEFAULT(0)
 	, brkwp_brkUser VARCHAR(20) NOT NULL CONSTRAINT DEF_brkwp_brkUser DEFAULT('')
 	, brkwp_brkDate DATETIME NOT NULL CONSTRAINT DEF_brkwp_brkDate DEFAULT(GETDATE())
