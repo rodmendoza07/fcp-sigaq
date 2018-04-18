f49307bf0e7fb8fcde2636a586bd7190391b2637
rodrigoMendoza <luis.mendoza@grupoporvenir.com.mx>
2018-04-18 11:22:33 -0500

    va

--- indices_tp_checklistwarranty.sql
+++ indices_tp_checklistwarranty.sql
@@ -3,11 +3,11 @@ Missing Index Details from SQLQuery1.sql - 192.168.1.10.master (procesador1 (69)
 The Query Processor estimates that implementing the following index could improve the query cost by 89.1299%.
 */
 
-/*
+
 USE [INVENTARIO]
 GO
 CREATE NONCLUSTERED INDEX [<Name of Missing Index, sysname,>]
 ON [dbo].[tp_checkListWarranty] ([sinv_id],[wlc_respStageUser])
 INCLUDE ([wlc_codeSVA],[wlc_amount],[wlc_createUser],[wlc_createDate])
 GO
-*/
+
